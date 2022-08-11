dadb5a970e48ac75c303fedfd1c14d5fbd13f754
Michael Dear <michaelcdear@gmail.com>
2022-08-11 11:22:16 -0600

    commit tree 30c529e3c0f5aa5b33e8fa4be696a1e70c6e6e20

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("")[self.split("").length - 1] == "." ? true : false
   end
 
   def question?
-
+    self.split("")[self.split("").length - 1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self.split("")[self.split("").length - 1] == "!" ? true : false
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.delete("")
+    array.length
   end
 end
